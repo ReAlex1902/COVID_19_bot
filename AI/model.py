43845ecd3ce81f0b3090c5e84e87eb4ae71f22e3
ReAlex1902 <alexrmadrid717@gmail.com>
2020-05-02 00:46:15 +0300

    AI upd

--- AI/model.py
+++ AI/model.py
@@ -7,17 +7,17 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(8, 64)
-        self.layers = [nn.Linear(64, 64)] * 3
+        self.layers = [nn.Linear(64, 64)] * 9
         self.fc12 = nn.Linear(64, 2)
 
     def return_one_number(self, result):
         """
         Helps to return not the number of prediction
         """
-        CONSTANT = -1.2             ## constant number to compare hthe prediction with
+        CONSTANT = - 1.2             ## constant number to compare hthe prediction with
         # lst = torch.Tensor(lst)     ## transforming to pytorch tensor
         # result = self.forward(lst)      ##
-        lst = [1 if elem[1] > CONSTANT else int(torch.argmax(elem)) for elem in result]    ## -1.39; -1.89
+        lst = [1 if elem[1] > CONSTANT else int(torch.argmax(elem)) for elem in result]    ## regulation of predictions
         return lst
 
     def forward(self, X):
@@ -28,7 +28,7 @@ class Net(nn.Module):
         for layer in self.layers:           ## predicting
             X = layer(X)
         X = self.fc12(X)
-        result = F.log_softmax(X)           ## writing results using log_softmax
+        result = F.log_softmax(X, dim = 1)           ## writing results using log_softmax
 
         return (self.return_one_number(result), result)     ## returning tuple with list of predictions and tensor of
                                                             ## log_softmax probabilities
--- AI/test.py
+++ AI/test.py
@@ -2,8 +2,6 @@
 from model import *
 
 loaded_model = get_model()
-print(loaded_model)
 
-
-lst = [1, 0, 0, 0, 0, 0, 0, 1]
+lst = [1, 0, 1, 0, 0, 0, 0, 1]
 print(loaded_model(lst))
