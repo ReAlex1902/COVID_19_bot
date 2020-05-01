a1b1fe3b424ec5fd8a40ac3730e3fd8b62b4507e
vadimkholodilo <vadimkholodilo@gmail.com>
2020-05-01 17:13:06 +0300

    Source code for a bot has been added

---
+++ bot/config.py
@@ -0,0 +1,1 @@
+token = 'my awesome token'
---
+++ bot/question.py
@@ -0,0 +1,5 @@
+class Question(object):
+	def __init__(self, question_text, internal_name):
+		self.question_text = question_text
+		self.internal_name = internal_name
+
