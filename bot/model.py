01ef294e79f0fa872d5cab23c39860b3d0d8bcc9
vadimkholodilo <vadimkholodilo@gmail.com>
2020-05-02 14:17:53 +0300

    Probability added

--- bot/model.py
+++ bot/model.py
@@ -7,7 +7,7 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(8, 64)
-        self.layers = [nn.Linear(64, 64)] * 3
+        self.layers = [nn.Linear(64, 64)] * 9
         self.fc12 = nn.Linear(64, 2)
 
     def return_one_number(self, result):
@@ -17,8 +17,11 @@ class Net(nn.Module):
         CONSTANT = -1.2             ## constant number to compare hthe prediction with
         # lst = torch.Tensor(lst)     ## transforming to pytorch tensor
         # result = self.forward(lst)      ##
-        lst = [1 if elem[1] > CONSTANT else int(torch.argmax(elem)) for elem in result]    ## -1.39; -1.89
+        lst = [1 if elem[1] > CONSTANT else int(torch.argmax(elem)) for elem in result]    ## regulation of predictions
         return lst
+
+    def return_proba(self, result):
+        return torch.exp(result)
 
     def forward(self, X):
         X = torch.Tensor(X)
@@ -28,14 +31,12 @@ class Net(nn.Module):
         for layer in self.layers:           ## predicting
             X = layer(X)
         X = self.fc12(X)
-        result = F.log_softmax(X)           ## writing results using log_softmax
+        result = F.log_softmax(X, dim = 1)           ## writing results using log_softmax
 
-        return (self.return_one_number(result), result)     ## returning tuple with list of predictions and tensor of
+        return (self.return_proba(result))     ## returning tuple with list of predictions and tensor of
                                                             ## log_softmax probabilities
 
 
 def get_model(filename = "net.pt"):
-    import pickle
-    import torch
     model = torch.load(filename)
     return model
