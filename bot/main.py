6f3dbd82c521891de5f3725b634b662f32bda429
vadimkholodilo <vadimkholodilo@gmail.com>
2020-05-05 01:40:49 +0300

    upd

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,13 @@
-import pickle
-import questions # A module which holds all questions. Then they will be moved to a database
-import keyboards
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, DictPersistence, CallbackQueryHandler
-import config
+import pickle
+from questions import questions # A module which holds all questions. Then they will be moved to a database
+import keyboards # This module has a lot of prebuilt keyboards. It is also a good example for Keyboard.py class
+import config # Bot's settings
 
 # helpers
-# function create_list creates list putting all data from the dictionary in the right order. Then this list is sent to the model
 def create_data_list(dic):
+	""" This function creates a list from a data dictionary where all user data is stored """
 	lst = []
 	lst.append(dic['cough'])
 	lst.append(dic['fever'])
@@ -20,37 +20,40 @@ def create_data_list(dic):
 	return lst
 
 def create_report(prediction, answers):
+	""" This function creates the report for a user based on prediction from the model """
 	report = 'Вы больны с вероятностью '
 	report += str(prediction)
 	report += '%'
+	report += '\n\n Рекомендации: \n'
 	if prediction > 30:
-		report += '\n\n Рекомендация: \n \
-		Вероятность того, что вы инфецированы слишком высока. Что делать: \n\
+		report += 'Вероятность того, что вы инфецированы слишком высока. Что делать: \n\
 		1. Сохранять спокойствие. 100% гарантии вам может дать только медицинский тест. \n\
-		2. Ждать звонка от врача. Втечении 24 часов с вами свяжется врач. \n\
-		3. Исключить любые контакты с родственниками и людьми вокруг.'
+		2. Ждать звонка от врача. Втечение 24 часов с вами свяжется врач. \n\
+		3. Исключить, на сколько это возможно, любые контакты с родственниками и людьми вокруг.'
+	else:
+		report += 'Хоть бот и вычислил достаточно малую вероятность того, что вы заражены, но это не значит, что вы действительно полностью здоровы. Помните, что самым точным показателем того, что вы здоровы, является тест на Каронавирус'
 	return report
 
 # Handlers
 def start(update, context): # handles /start command which is sent automaticly when you write to the bot
-	print('Start callback has been called')
-	#print('Saved user data', context.user_data)
-	#print('Update object: ', update)
 	update.message.reply_text('Добро пожаловать в COVID-19 diagnostic bot. \n\n\
 	Данный бот задаст вам несколько вопросов, которые помогут оценить врачам ваше состояние. \
 	Также, когда вы закончите вводить свои данные, бот вычислит вероятность заболевания. \n\n\
 	Пожалуйста, отвечайте на вопросы честно. \n\n\
 	Для работы с ботом, вы можете использовать следующие команды: \n\
 	/test - начать тестирование, \n\
-	/help - справка по боту. \n\n\
 	Берегите себя.')
 
 def test(update, context): # handles /test command
-	update.message.reply_text('Тестирование начато.')
+	update.message.reply_text('Тестирование начато. \n\n \
+	Внимание! Бот собирает конфеденциальную информацию: \
+	имя, фамилию, номер мобильного телефона и адрес при необходимости \n \
+	Данная информация собирается ботом в целях связи с вами и никогда не будет передана третьим лицам без вашего на то согласия \n \
+	Продолжая пользоваться ботом, вы принимаете данные условия.')
 	context.user_data['is_testing'] = True
 	context.user_data['question_index'] = 0
 	context.user_data['answers'] = {}
-	msg = update.message.reply_text(questions.questions[0].question_text)
+	msg = update.message.reply_text(questions[0].question_text)
 	chat_id = msg.chat_id
 	message_id = msg.message_id
 	context.chat_data['message_id'] = message_id
@@ -69,26 +72,21 @@ def echo(update, context): # Handles all messages and buttons
 			context.chat_data['message_id'] = -1
 			context.chat_data['chat_id'] = -1
 		question_index = context.user_data['question_index']
-		if questions.questions[question_index].answer_type == 'bool':
-			#answer = questions.get_bool_value(answer)
-			if answer == -1:
-				message.reply_text('Неверное значение. Напишите да или нет')
-				return
-		context.user_data['answers'][questions.questions[question_index].internal_name] = answer
+		context.user_data['answers'][questions[question_index].internal_name] = answer
 		question_index = question_index + 1
 		context.user_data['question_index'] = question_index
-		if question_index <= len(questions.questions) - 1:
-			if questions.questions[question_index].answer_type == 'bool':
-				keyboard = questions.questions[question_index].keyboard
+		if question_index <= len(questions) - 1:
+			if questions[question_index].answer_type == 'bool':
+				keyboard = questions[question_index].keyboard
 				reply_markup = InlineKeyboardMarkup(keyboard)
 				if context.chat_data['message_id'] != -1 and context.chat_data['chat_id'] != -1:
-					context.bot.edit_message_text(chat_id=context.chat_data['chat_id'], message_id=context.chat_data['message_id'], text=questions.questions[question_index].question_text, reply_markup=reply_markup)
+					context.bot.edit_message_text(chat_id=context.chat_data['chat_id'], message_id=context.chat_data['message_id'], text=questions[question_index].question_text, reply_markup=reply_markup)
 				else:
-					msg = message.reply_text(questions.questions[question_index].question_text, reply_markup=reply_markup)
+					msg = message.reply_text(questions[question_index].question_text, reply_markup=reply_markup)
 					context.chat_data['chat_id'] = msg.chat_id
 					context.chat_data['message_id'] = msg.message_id
 			else:
-				context.bot.edit_message_text(chat_id=context.chat_data['chat_id'], message_id=context.chat_data['message_id'], text=questions.questions[question_index].question_text)
+				context.bot.edit_message_text(chat_id=context.chat_data['chat_id'], message_id=context.chat_data['message_id'], text=questions[question_index].question_text)
 		else:
 			context.bot.edit_message_text(chat_id=context.chat_data['chat_id'], message_id=context.chat_data['message_id'], text='Тестирование завершено')
 			context.user_data['is_testing'] = False
--- bot/questions.py
+++ bot/questions.py
@@ -1,16 +1,16 @@
 import keyboards
-import question
+from question import Question
 questions = []
 
-questions.append(question.Question('Напишите ваше полное имя?', 'full_name', 'text'))
-questions.append(question.Question('Какая у вас температура?', 'fever', 'bool', keyboards.temperature_selector))
-questions.append(question.Question('Оцените ваш кашель', 'cough', 'bool', keyboards.cough_scale))
-questions.append(question.Question('Оцените ваш уровень одышки', 'shortness_of_breath', 'bool', keyboards.shortness_of_breath_scale))
-questions.append(question.Question('Насколько сильно у вас болит голова?', 'head_ache', 'bool', keyboards.pain_scale))
-questions.append(question.Question('Насколько сильно у вас болит горло?', 'sore_throat', 'bool', keyboards.pain_scale))
-questions.append(question.Question('Укажите ваш возраст', 'age', 'text'))
-questions.append(question.Question('Какой ваш пол?', 'gender', 'bool', keyboards.gender_selector))
-questions.append(question.Question('Выберите одно утверждение из списка ниже, которое относится к вам', 'additional_factor', 'bool', keyboards.additional_factors))
+questions.append(Question('Напишите ваше полное имя?', 'full_name', 'text'))
+questions.append(Question('Какая у вас температура?', 'fever', 'bool', keyboards.temperature_selector))
+questions.append(Question('Оцените ваш кашель', 'cough', 'bool', keyboards.cough_scale))
+questions.append(Question('Оцените ваш уровень одышки', 'shortness_of_breath', 'bool', keyboards.shortness_of_breath_scale))
+questions.append(Question('Насколько сильно у вас болит голова?', 'head_ache', 'bool', keyboards.pain_scale))
+questions.append(Question('Насколько сильно у вас болит горло?', 'sore_throat', 'bool', keyboards.pain_scale))
+questions.append(Question('Укажите ваш возраст', 'age', 'text'))
+questions.append(Question('Какой ваш пол?', 'gender', 'bool', keyboards.gender_selector))
+questions.append(Question('Выберите одно утверждение из списка ниже, которое относится к вам', 'additional_factor', 'bool', keyboards.additional_factors))
 
 
 #  function get_bool_value converts words to boolean values. For example, yes or YES or Yes will be converted to 1 or True
