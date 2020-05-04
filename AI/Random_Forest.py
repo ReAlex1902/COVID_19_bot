0b8ef0ce3e351d1acec0f4f856a5fea868e0a471
ReAlex1902 <alexrmadrid717@gmail.com>
2020-05-04 21:23:43 +0300

    boosting model

--- AI/Random_Forest.py
+++ AI/Random_Forest.py
@@ -4,7 +4,7 @@ import pickle
 filename = "Random_Forest.sav"
 rf = pickle.load(open(filename, "rb"))
 
-lst = [2, 5, 1, 0, 0, 37, 1, 0]
+lst = [2, 7, 0, 1, 0, 55, 1, 0]
 result = rf.predict_proba([lst])
 
 print(result)
